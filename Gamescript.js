6a159401a01df0f09fb9dfc35a6d43c8aa074223
Vera869 <verea8910@gmail.com>
2023-09-02 15:57:21 +0700

    Game function

---
+++ Gamescript.js
@@ -0,0 +1,24 @@
+
+//let monthNumber = Number(prompt("Укажите номер месяца"));
+
+ function gameMonth () {
+   let monthNumber = Number(prompt("Укажите номер месяца"));
+   if (monthNumber < 0 || monthNumber > 13) {
+      console.log("Номер месяца введён не корректно!");
+   } else {
+      if (monthNumber === 12 || monthNumber < 3) {
+         console.log("Зима");
+      } else {
+         if (monthNumber >=3 && monthNumber <= 5) {
+            console.log("Весна");
+         } else {
+            if (monthNumber >=6 && monthNumber <= 8) {
+               console.log("Лето");
+            } else {
+               console.log("Осень");
+            }
+         }
+      }
+   }
+ }
+ //console.log(gameMonth());     
